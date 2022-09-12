1a2b31cbf1570ef7a2670320b760ffe4868bb05f
Your Name <samaneh.rezaie74@gmail.com>
2022-09-12 10:56:05 +0430

    start new project linked share

---
+++ src/app/sharelinks/sharelinks.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { SharelinksRoutingModule } from './sharelinks-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    SharelinksRoutingModule
+  ]
+})
+export class SharelinksModule { }
