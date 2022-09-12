fec6715c1ffb2edd2184636a4d5348659bde9e03
samaneh rezaie <samaneh.rezaie74@gmail.com>
2022-09-12 22:40:53 +0430

    read posts

--- src/app/sharelinks/pages/posts/posts.component.ts
+++ src/app/sharelinks/pages/posts/posts.component.ts
@@ -1,5 +1,11 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { observable, Observable } from 'rxjs';
 import { PostApiService } from '../../http/post-api.service';
+import { Posts } from '../../models/posts.model';
+import { environment } from 'src/environments/environment';
+// import {data} from '../../../../assets/data/posts.json';
+
 
 @Component({
   selector: 'app-posts',
@@ -10,13 +16,14 @@ export class PostsComponent implements OnInit {
   posts:any;
 
 
-  constructor(private postsApi:PostApiService) { }
+
+  constructor(private postsApi:PostApiService,private http:HttpClient) { }
 
   ngOnInit(): void {
 
-    this.posts=this.postsApi.getposts().subscribe(result => 
-      {this.posts=result
-        console.log(this.posts)
+    this.http.get(environment.url).subscribe(result => 
+      {this.posts=result 
+        
       });
   }
 
