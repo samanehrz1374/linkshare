2f7451f9cf5c2f1efa5e7d5ab0089d6ffdc6903e
samanehrz1374 <samaneh.rezaie74@gmail.com>
2022-10-07 13:59:18 +0330

    add user name check in register

--- src/app/auth/http/auth.service.ts
+++ src/app/auth/http/auth.service.ts
@@ -22,7 +22,31 @@ export class AuthService {
         
       }else{
         this.status=403;
+        
+      }
+    }
+    return this.status;
+
+
+
+
+
+  }
+
+
+  userName_exist(users:any,formdata:any,){
+    
+    for (let i=0; i<users.length;i++){
+      
+        
+      if (users[i].userName===formdata.userName){
+        
+        this.status=200;
         break
+        
+      }else{
+        this.status=403;
+        
       }
     }
     return this.status;
--- src/app/auth/pages/register/register.component.ts
+++ src/app/auth/pages/register/register.component.ts
@@ -4,6 +4,7 @@ import { FormControl, FormGroup, NgForm, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { BsModalRef, BsModalService } from 'ngx-bootstrap/modal';
 import { environment } from 'src/environments/environment';
+import { AuthService } from '../../http/auth.service';
 
 @Component({
   selector: 'app-register',
@@ -25,9 +26,11 @@ export class RegisterComponent implements OnInit {
   }
   modalRef: BsModalRef;
   is_password_notEqual:boolean=false;
+  is_userName_exist:boolean=false;
+  status:number=0;
 
 
-  constructor(private http:HttpClient,private route:Router,private modalService: BsModalService) { }
+  constructor(private loginservise:AuthService,private http:HttpClient,private route:Router,private modalService: BsModalService) { }
 
   ngOnInit(): void {
 
@@ -44,8 +47,8 @@ export class RegisterComponent implements OnInit {
       "firstName":new FormControl('سمانه'),
       "lastName":new FormControl('رضایی'),
       "userName":new FormControl('saman',[Validators.required,Validators.pattern('[a-zA-Z ]*')]),
-      "password":new FormControl('123456',[Validators.required,Validators.pattern('^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$')]),
-      "confirm_password":new FormControl('123456',[Validators.required,Validators.pattern('^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$')]),
+      "password":new FormControl('159753Mm@',[Validators.required,Validators.pattern('^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$')]),
+      "confirm_password":new FormControl('159753Mm@',[Validators.required,Validators.pattern('^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$')]),
       "userProfile":new FormControl('http://localhost:4200/assets/images/user_images/user5.jpg'),
       "email":new FormControl('s@gmail.com',[Validators.required,Validators.email])
     })
@@ -53,25 +56,42 @@ export class RegisterComponent implements OnInit {
 
   onRejisterSubmit(formdata:any){
 
-    if(formdata.password === formdata.confirm_password){
+    this.http.get(environment.userUrl).subscribe((result:any)=>{
+      this.users=result;
 
-      this.newUser={
-        "userName":`${formdata.userName}`,
-        "userProfile":`${formdata.userProfile}`,
-        "firstName":`${formdata.firstName}`,
-        "lastName":`${formdata.lastName}`,
-        "password":`${formdata.password}`,
-        "email":`${formdata.email}`,
-  
+      this.status=this.loginservise.userName_exist(this.users,formdata)
+
+      if (this.status===200){
+        console.log(this.status)
+        
+        this.is_userName_exist=true;
+
       }
+      if(this.is_userName_exist===false){
   
-      this.users.push(this.newUser)
-      this.logged_in_username.emit(this.newUser)
+        if(formdata.password === formdata.confirm_password){
+    
+          this.newUser={
+            "userName":`${formdata.userName}`,
+            "userProfile":`${formdata.userProfile}`,
+            "firstName":`${formdata.firstName}`,
+            "lastName":`${formdata.lastName}`,
+            "password":`${formdata.password}`,
+            "email":`${formdata.email}`,
       
-      // this.route.navigate([''])  
-    }else{
-      this.is_password_notEqual=true;
-    }
+          }
+      
+          this.users.push(this.newUser)
+          this.logged_in_username.emit(this.newUser)
+          
+          // this.route.navigate([''])  
+        }else{
+          this.is_password_notEqual=true;
+        }
+      }
+    })
+
+
   
 
 
